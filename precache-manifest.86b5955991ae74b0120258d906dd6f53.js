47942327cec5a2041c9c00c4a2c9f2b33e2554fa
alexey <alex.vasilenko13@gmail.com>
2020-04-16 00:53:21 +0300

    Updates

---
+++ precache-manifest.86b5955991ae74b0120258d906dd6f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c34383b7bf6601d1b35d",
+    "url": "/reactwarriors-master-class/static/js/runtime~main.199926b5.js"
+  },
+  {
+    "revision": "c3c4c6197d61ac334ad2",
+    "url": "/reactwarriors-master-class/static/js/main.26d0e876.chunk.js"
+  },
+  {
+    "revision": "4dd1e7de9820b0f2cb3c",
+    "url": "/reactwarriors-master-class/static/js/2.46fa7967.chunk.js"
+  },
+  {
+    "revision": "c3c4c6197d61ac334ad2",
+    "url": "/reactwarriors-master-class/static/css/main.396f8bfd.chunk.css"
+  },
+  {
+    "revision": "4dd1e7de9820b0f2cb3c",
+    "url": "/reactwarriors-master-class/static/css/2.f2d17a84.chunk.css"
+  },
+  {
+    "revision": "8e062dd15f34df086038b9606f248c5b",
+    "url": "/reactwarriors-master-class/index.html"
+  }
+];
